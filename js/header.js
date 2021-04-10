939bc35d0a089f7358accb455c7629d2dec044c1
DarioMarti <77503520+DarioMarti@users.noreply.github.com>
2021-04-10 14:57:49 +0200

    bien

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ var cabecera = document.getElementById('header');
 window.addEventListener('scroll',headercolor);
 function headercolor(){
 	var top = window.scrollY;
-	if(top >= 900){
+	if(top >= 800){
 	cabecera.style.backgroundColor="black";
 	console.log('hey, estas escroleando');
 	}else{
