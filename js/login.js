9998532db5a0f93ffc6ea258aaefd910d941bc11
DarioMarti <77503520+DarioMarti@users.noreply.github.com>
2021-04-23 15:28:32 +0200

    new index

---
+++ js/login.js
@@ -0,0 +1,7 @@
+
+document.getElementById('enviar').addEventListener('click', envio);
+function envio(){
+	window.open('inicio.html','_top');
+}
+var fotito = document.getElementById('fotoPerf').value;
+console.log(fotito);
---
+++ js/registro.js
@@ -0,0 +1,8 @@
+document.getElementById('login').addEventListener('click', loguear);
+function loguear(){
+	window.open('login.html','_top');
+}
+document.getElementById('registro').addEventListener('click', registrar);
+function registrar(){
+	window.open('regis.html','_top');
+}
