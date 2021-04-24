ff0799738517f5f657b10ffcca22b71c6391b376
DarioMarti <77503520+DarioMarti@users.noreply.github.com>
2021-04-24 20:26:56 +0200

    reprod
    re

---
+++ js/script.js
@@ -0,0 +1,180 @@
+
+/*document.getElementById('AbrirVolumen').addEventListener('click', abrirvol);
+function abrirvol(){
+	document.getElementById('volumen').style.backgroundColor = "red";
+}*/
+//Array con el listado de canciones a mostrar en el reprodutor
+const canciones = [
+"Nuevos-días.mp3",
+"Your-Light.mp3",
+"Copenhague.mp3",
+"Club-de-fans-de-John-Boy.mp3",
+"De-la-monarquía-a-la-criptocracia.mp3",
+"I-Want-The-World-To-Stop.mp3",
+"Gaia.mp3"
+]
+
+var indiceActual = new Array(1)
+//Funcion para crear mediante javascript el listado de canciones
+function crearPlayList(){
+	const listado = document.createElement('ol')
+	listado.setAttribute("id", 'listadoMusica')
+	for (let i = 0; i<canciones.length; i++){
+		const item = document.createElement('li')
+		item.appendChild(document.createTextNode(canciones[i])) 
+		item.setAttribute("id", canciones.indexOf(canciones[i]))
+		listado.appendChild(item)
+	}
+	return listado
+}
+document.getElementById('playList').appendChild(crearPlayList())
+
+var listadoMusica= document.getElementById('listadoMusica')
+listadoMusica.onclick = (e) =>{
+	const itemClick = e.target
+	removeActive()
+	itemClick.classList.add("active");
+	reproduccionActual( itemClick.innerText)
+	loadMusic(itemClick.innerText)
+	player.play()
+	indiceActual[0]= e.target.id
+	classIconPlay();
+
+}
+//Funcion para cambiar el icono del reprodutor
+function classIconPlay(){
+	var element = document.getElementById("iconPlay")
+	element.classList.remove("fa-play");
+    element.classList.add("fa-pause");
+}
+//Funcion para control del volumen
+const volumen= document.getElementById("volumen")
+volumen.oninput= (e) =>{
+	const vol = e.target.value
+	player.volume =vol
+}
+
+//Funcion para actualizar la barra de progreso del reprodutor
+document.getElementById('timer').innerText='0:00 / 0:00';
+const updateProgress = () =>{
+	if (player.currentTime >0){
+		const barra = document.getElementById('progress')
+		barra.value = (player.currentTime / player.duration) * 100
+		
+		var duracionSegundos= player.duration.toFixed(0);
+		dura=secondsToString(duracionSegundos);
+		var actualSegundos = player.currentTime.toFixed(0)
+		actual=secondsToString(actualSegundos);
+		
+		duracion= actual +' / '+ dura
+		document.getElementById('timer').innerText=duracion 
+	}
+	if (player.ended){
+		nextMusic();//Reproducir la siguiente pista
+	} 
+}
+//Funcion para reproducir la proxima cancion
+function nextMusic(){  
+	const source = document.getElementById('source');
+	var musicaActual= Number(indiceActual[0]);
+	if (canciones.length == (musicaActual+1)){
+		var siguiente = 0
+	} else {
+		var siguiente = musicaActual + 1
+	}
+	removeActive()
+	var item=document.getElementById(siguiente)
+	item.classList.add("active");
+	loadMusic(canciones[siguiente]);
+	player.play()
+	indiceActual[0]= siguiente
+	reproduccionActual( canciones[siguiente])
+	classIconPlay()
+}
+//Funcion para reproducir la cancion anterior
+function prevMusic(){  
+	const source = document.getElementById('source');
+	var musicaActual= Number(indiceActual[0]);
+	if (musicaActual==0){
+		var anterior= canciones.length - 1
+	} else {
+		var anterior = musicaActual - 1
+	}
+	removeActive()
+	var item=document.getElementById(anterior)
+	item.classList.add("active");
+	loadMusic(canciones[anterior]);
+	player.play()
+	indiceActual[0]= anterior
+	reproduccionActual( canciones[anterior])
+	classIconPlay()
+}
+//Funcion para remover todas las clases css activas
+function removeActive(){
+	var elems = document.querySelectorAll(".active");
+ 	 [].forEach.call(elems, function(el) {
+    	el.classList.remove("active");
+ 	 });
+ 	 return elems
+}
+//Funcion para mostrar el nombre del arhivo actual en reproduccion
+function reproduccionActual(texto){
+	document.getElementById('currentPlay').innerText=texto
+}
+//Funcion para cargar las canciones en el reproductor
+function loadMusic(ruta){
+	var source = document.getElementById('source')
+	var folder ="audio";//Carpeta donde tenemos almancenada la musica
+	source.src= folder+"/"+ruta
+	var index= indiceActual[0]= canciones.indexOf(ruta)
+	removeActive()
+	var item=document.getElementById(index)
+	item.classList.add("active");
+	reproduccionActual( ruta)
+	player.load();
+	portadita();
+}
+//Funcion para pausar o darle play 
+function togglePlay() {
+	if (player.paused){
+		toggleIcon();
+		return player.play();
+	} else {
+		toggleIcon();
+		return player.pause();
+	}
+}
+//Funcion para cambiar el icono play o pause
+function toggleIcon() {
+   var element = document.getElementById("iconPlay");
+   element.classList.toggle("fa-pause");
+   element.classList.toggle("fa-play");
+}
+//Funcion para que al dar click sobre la barra de progeso se permita adelantar
+progress.addEventListener('click', adelantar);
+function adelantar(e){
+	const scrubTime = (e.offsetX / progress.offsetWidth) * player.duration;
+	player.currentTime = scrubTime;
+	sonsole.log(e);
+}
+//Funcion para convertir segundos a minutos y horas
+function secondsToString(seconds) {
+	var hour="";
+	if (seconds>3600){
+		hour = Math.floor(seconds / 3600);
+		hour = (hour < 10)? '0' + hour : hour;
+		hour+=":"
+	}
+   var minute = Math.floor((seconds / 60) % 60);
+  minute = (minute < 10)? '0' + minute : minute;
+  var second = seconds % 60;
+  second = (second < 10)? '0' + second : second;
+  return hour  + minute + ':' + second;
+}
+loadMusic(canciones[0]);
+function portadita(){
+var portada =document.getElementById('portadaDisco');
+var raiz = "img/"+currentPlay.innerText+".jpg";
+portada.style.backgroundImage = 'url('+raiz+')';
+}
+
