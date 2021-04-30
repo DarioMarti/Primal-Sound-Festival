bfacab57980d384357da4f6b5311e4b5654a2e8a
DarioMarti <77503520+DarioMarti@users.noreply.github.com>
2021-04-30 16:56:00 +0200

    dsas

--- js/js.js
+++ js/js.js
@@ -5,5 +5,7 @@ document.getElementById('buscador').addEventListener('click',function busca(){
 document.getElementById('buscadorMovil').addEventListener('click',function buscaMovil(){
 	document.getElementById('franjaBuscador').classList.toggle('buscando');
 })
-
+document.getElementById('editado').addEventListener('click',function editar(){
+	document.getElementById('editarProfile').classList.toggle('editando');
+})
 
